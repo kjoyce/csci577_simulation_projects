5a749de35843fcc4464d7e29d7f8dbca3ef54dfe
Kevin Joyce <kevin.t.joyce@gmail.com>
2013-03-14 19:47:50 -0600

    First Commit

---
+++ project1_molecular_friction/Initialize.py
@@ -0,0 +1,43 @@
+from Container import Container
+from Force import LeonardJonesForce
+from Integrator import VerletIntegrator
+from DistanceMatrix import DistanceMatrix
+from numpy import linspace,sqrt,mod,inf,sin,pi
+from pylab import rand,randint
+from Container import Container
+class ParticleInitialize(object):
+  def __init__(self,case):
+    dims = 2
+    dt = .01
+    self.distance_matrix = DistanceMatrix()
+    self.force		 = LeonardJonesForce(dims,self.distance_matrix)
+    self.integrate	 = VerletIntegrator(dt,self.force)
+    self.c		 = Container(self.integrate)
+    c = self.c
+    initialization = case
+    xlim = (0,18)
+    ylim = (-1,5)
+    pot_energy_lim = (-10,30)
+    kin_energy_lim = (40,70)
+    tot_energy_lim = (40,70)
+    pressure_lim = (40,70)
+    
+    dx = 2**(1./3.)
+    dy = sin(pi/3.)*dx
+    for i in range(100):
+      c.addParticle(float(i)*dx,0.,0.,0.,1.)
+
+    for i in range(100,113):
+      print "{}, {}".format((i-99)*dx,dy*i%2 + dx)
+      c.addParticle((i-99)*dx,dy*(i%2) + dx,0.,0.,1.)
+
+    self.xlim            = xlim
+    self.ylim            = ylim
+    self.pot_energy_lim  = pot_energy_lim
+    self.kin_energy_lim  = kin_energy_lim
+    self.tot_energy_lim  = tot_energy_lim
+    self.pressure_lim    = pressure_lim
+  def __call__(self):
+    return self.c,self.distance_matrix,self.force,self.integrate,self.xlim,self.ylim,self.pot_energy_lim,self.kin_energy_lim,self.tot_energy_lim,self.pressure_lim
+
+
---
+++ project1_molecular_friction/driver.py
@@ -0,0 +1,26 @@
+from Animation import Container_Animation
+from Initialize import ParticleInitialize
+########## PARAMS ###############
+num_frames = 200
+ekg_length = 80
+delay      = 0
+vid_format = ".mp4"
+
+run_backwards  = False
+save_animation = False
+print_frame    = False
+tile_domain    = False
+crunch_domain  = False
+
+case = "building"
+
+initializer = ParticleInitialize(case)
+c,distance_matrix,force,integrate,xlim,ylim,pot_energy_lim,kin_energy_lim,tot_energy_lim,pressure_lim = initializer()
+
+filename = case
+animator = Container_Animation(c,integrate,force,xlim,ylim,pot_energy_lim,kin_energy_lim,tot_energy_lim,pressure_lim,run_backwards=run_backwards,save_animation=save_animation,print_frame=print_frame,tile_domain=tile_domain,crunch_domain=crunch_domain,vid_format=vid_format,filename=filename,num_frames=num_frames,ekg_length=ekg_length)
+
+if save_animation:
+  animator.save()
+else:
+  animator.show()
